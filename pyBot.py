fd6397f78371e9c60c342d26f69e4aa840b59709
ampoz <hampoz@gmail.com>
2013-07-05 14:48:18 -0400

    + Commented all psutil stuff because it now only compiles if you have VS2008 installed. Gives error other wise. Is there another more general module we could use? Or I'll just install VS2008 when I get home. + Added join and part commands so the bot can join and part channels (except lobby). + Implemented alternate nickname so that it is used if the primary nickname is already taken. % Modified help command to reflect this. % Modified BI, UD, ABOUT, HELP % Fixed some english grammatical errors (lol)

--- pyBot.py
+++ pyBot.py
@@ -7,13 +7,15 @@ import signal
 import re
 import ConfigParser
 import os
-import psutil
+#import psutil
 import platform
 
 from bs4 import BeautifulSoup
 from time import sleep
 
-def BI():
+def BI(keyword):
+    chat = str(keyword)
+    
     url = "http://www.businessinsider.com"
     data = urllib2.urlopen(url).read()
     bs = BeautifulSoup(data)
@@ -24,9 +26,11 @@ def BI():
     for index, title in enumerate(titles):
         if index >= 5:
                 break
-        s.send("PRIVMSG %s :%s\r\n" %  (LOBBY, (title.get_text()).encode('ascii', 'ignore')))
+        s.send("PRIVMSG %s :%s\r\n" %  (chat, (title.get_text()).encode('ascii', 'ignore')))
 
-def UD():
+def UD(keyword):
+    chat = str(keyword)
+    
     url = "http://www.urbandictionary.com/random.php"
     data = urllib2.urlopen(url).read()
     bs = BeautifulSoup(data)
@@ -38,7 +42,7 @@ def UD():
 
     word = word.strip()
     definition = definition.strip()
-    s.send("PRIVMSG %s :%s : %s\r\n" % (LOBBY, word, definition))
+    s.send("PRIVMSG %s :%s : %s\r\n" % (chat, word, definition))
 
 def UDD(keyword):
     if(str(keyword) is None):
@@ -66,31 +70,60 @@ def UDD(keyword):
         definition = definition.strip()
         s.send("PRIVMSG %s :%s : %s\r\n" % (LOBBY, word, definition))
 
-def ABOUT():
-    pid = os.getpid()
-    p = psutil.Process(pid)
-    cpu = p.get_cpu_percent(interval=1.0)
-    mem = p.get_memory_percent()
+def ABOUT(keyword):
+    chat = str(keyword)
+    
+    #pid = os.getpid()
+    #p = psutil.Process(pid)
+    #cpu = p.get_cpu_percent(interval=1.0)
+    #mem = p.get_memory_percent()
     sys = platform.system()
     #ver = platform.version()
     layout = platform.machine()
-    s.send("PRIVMSG %s : %s v%s\r\n" % (LOBBY, NICK, VERSION))
-    s.send("PRIVMSG %s : I'm running on %s (%s).\r\n" % (LOBBY, sys, layout))
-    s.send("PRIVMSG %s : Currently using %0.2f%% of the CPU and %0.2f%% of RAM.\r\n" % (LOBBY, cpu, mem))
+    s.send("PRIVMSG %s : %s v%s\r\n" % (chat, NICK, VERSION))
+    s.send("PRIVMSG %s : I'm running on %s (%s).\r\n" % (chat, sys, layout))
+    #s.send("PRIVMSG %s : Currently using %0.2f%% of the CPU and %0.2f%% of RAM.\r\n" % (LOBBY, cpu, mem))
 
 
-def HELP():
+def HELP(keyword):
+    chat = str(keyword)
+    
     #BI
     command = "~BI"
-    s.send("PRIVMSG %s : Top Business Insider Story %s\r\n" % (LOBBY, command))
+    s.send("PRIVMSG %s : Top Business Insider Stories. %s\r\n" % (chat, command))
 
     #UD
     command = "~UD"
-    s.send("PRIVMSG %s : Gives a random Urban Dictionary definition %s\r\n" % (LOBBY, command))
+    s.send("PRIVMSG %s : Gives a random Urban Dictionary definition. %s\r\n" % (chat, command))
 
     #UDD
     command = "~UDD _keyword_"
-    s.send("PRIVMSG %s : Returns the Urban Dictionnary Definition of a specific word %s\r\n" % (LOBBY, command))
+    s.send("PRIVMSG %s : Returns the Urban Dictionnary Definition of a specific word. %s\r\n" % (chat, command))
+
+    #JOIN
+    command = "~JOIN"
+    s.send("PRIVMSG %s : Joins the specified channel. %s\r\n" % (chat, command))
+    
+    #PART
+    command = "~PART"
+    s.send("PRIVMSG %s : Parts the channel in which the command was typed. %s\r\n" % (chat, command))
+    
+
+def JOIN(keyword): # Join a channel
+    if(str(keyword) is None):
+        keyword = LOBBY
+    
+    # Join lobby
+    print("Joining: %s" % keyword)
+    s.send("JOIN %s\r\n" % keyword)
+
+def PART(keyword): # Join a channel
+    # TODO: Do nothing if keyword is empty.. but this should never happen.
+    
+    # Part channel where command was typed. Unless it's the lobby or a user chat.
+    if(str(keyword)!=LOBBY):
+        print("Parting: %s" % keyword)
+        s.send("PART %s\r\n" % keyword)
 
 def signal_handler(signal, frame):
     print("ByeBye")
@@ -101,24 +134,43 @@ def connect():
     global s
     s=socket.socket()
     s.connect((HOST, PORT))
-
+    
+    print("Setting USER")
+    s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
+    
     print("Setting NICK")
     s.send("NICK %s\r\n" % NICK)
 
+    # Check incoming socket buffer to see if the nickname has already been used.
     readbuffer=s.recv(1024)
     sBuff=string.split(readbuffer, "\n")
     readbuffer=sBuff.pop( )
+    
+    for line in sBuff:
+        line=string.rstrip(line)
+        line=string.split(line)
+        print("LINE: " + line[0] + " " + line[1] + " " + line[2] + " " + line[3] + " " + line[4])
+        if(line[0]=="PING"):
+            s.send("PONG %s\r\n" % line[1])
+            print("Sending initial pong")
+        if(len(line) >= 5):
+            if(line[4]==":Nickname"):
+                s.send("NICK %s\r\n" % NICKALTER)
+                print("Nickname " + NICK + " already taken. Trying alternate nickname " + NICKALTER + ".")
 
+    # Re-Check the buffer for a new ping and an answer from our potential NICK change
+    readbuffer=s.recv(1024)
+    sBuff=string.split(readbuffer, "\n")
+    readbuffer=sBuff.pop( )
+    
     for line in sBuff:
         line=string.rstrip(line)
         line=string.split(line)
         if(line[0]=="PING"):
             s.send("PONG %s\r\n" % line[1])
             print("Sending initial pong")
-
-    print("Setting USER")
-    s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-
+    
+    # Join lobby
     print("Joining: %s" % LOBBY)
     s.send("JOIN %s\r\n" % LOBBY)
 
@@ -167,19 +219,19 @@ def get_commands():
                     
                     if(command.upper() == "~HELP"):
                         print("Help command from " + nickname + " in chat with " + chat)
-                        HELP()
+                        HELP(chat)
 
                     if(command.upper() == "~BI"):
                         print("BI command from " + nickname + " in chat with " + chat)
-                        BI()
+                        BI(chat)
 
                     if(command.upper() == "~UD"):
                         print("UD command from " + nickname + " in chat with " + chat)
-                        UD()
+                        UD(chat)
 
                     if(command.upper() == "~ABOUT"):
-                        print("ABOUT")
-                        ABOUT()
+                        print("ABOUT command from " + nickname + " in chat with " + chat)
+                        ABOUT(chat)
 
                     if(command.upper() == "~UDD"):
                         if(len(line) >= 5):
@@ -189,13 +241,25 @@ def get_commands():
                             print("UDD command with no arg from " + nickname + " in chat with " + chat)
                             s.send("PRIVMSG %s :No keyword entered. Try again\r\n" % chat)
                             # NOTE: Could simply merge UD and UDD user commands so that if the user puts an argument to ~UD, he gets the definition, and no argument gets a random definition.
-
+                    
+                    if(command.upper() == "~JOIN"):
+                        if(len(line) >= 5):
+                            print("JOIN command with arg \"" + line[4] + "\" from " + nickname + " in chat with " + chat)
+                            JOIN(' '.join(line[4:]).strip())
+                        else:
+                            print("JOIN command with no arg from " + nickname + " in chat with " + chat)
+                            s.send("PRIVMSG %s :No channel name entered. Try again\r\n" % chat)
+                    
+                    if(command.upper() == "~PART"):
+                        print("PART command from " + nickname + " in chat with " + chat)
+                        PART(chat)
+        
         except SystemExit:
             sys.exit(0)
 
         except:
             print("Error - ", sys.exc_info()[0], sys.exc_info()[1])
-            s.send("PRIVMSG %s :(eror 0x2381ff64) Look at that pipe, it's broken. Try again.\r\n" % LOBBY)
+            s.send("PRIVMSG %s :(error 0x2381ff64) Look at that pipe, it's broken. Try again.\r\n" % LOBBY)
 
 def main():   
     signal.signal(signal.SIGINT, signal_handler)
